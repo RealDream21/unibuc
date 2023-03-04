2b26b474bbc48b2a224526158e6e52584a80df5d
Fabi <fabianflorete@yahoo.com>
2023-03-04 18:19:35 +0200

    POO gayming

--- semestru2an1/POO/playground/main.cpp
+++ semestru2an1/POO/playground/main.cpp
@@ -1,42 +1,15 @@
 #include <iostream>
+#include <new>
 
 using namespace std;
 
-class Test
-{
-    int nota;
-    string text;
-
-public:
-    Test(int nr)
-    {
-        nota = nr;
-    }
-    Test(int nr, string s)
-    {
-        nota = nr;
-        text = s;
-    }
-    Test()
-    {
-        nota = 4;
-    }
-    int getNota()
-    {
-        return nota;
-    }
-    friend string getText(Test*);
-};
-
-string getText(Test* xd)
-{
-    return (*xd).text;
-}
-
-
 int main()
 {
-    Test mate(4, "rauuul");
-    cout << getText(&mate);
+    int a;
+    int *b = &a;
+    a = 10;
+    (*b)++;
+    cout << a << (*b);
+
     return 0;
 }
