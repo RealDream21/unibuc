42496ae994cf71af67fe617862a9e2a36bb8bdbb
Fabi <fabianflorete@yahoo.com>
2023-03-22 16:09:28 +0200

    4.4 tw

--- semestru2an1/TW/team4TW/4.4.js
+++ semestru2an1/TW/team4TW/4.4.js
@@ -14,14 +14,14 @@ console.log(informatii);
 
 let varstaMinima = prompt("Dati varsta minima");
 
-let filteredInformatii = informatii.filter((element, index) => element.age > varstaMinima);
+let filteredInformatii = informatii.filter((element, index) => parseInt(element.age) > parseInt(varstaMinima));
 
 
 if(filteredInformatii.length > 0){
   //alert(filteredInformatii.length + " persoane: ");
   toAlert = filteredInformatii.length + " persoane: "
   for(persoana of filteredInformatii){
-    toAlert += persoana
+    toAlert += persoana.name + " ";
     console.log(persoana);
   }
   alert(toAlert);
