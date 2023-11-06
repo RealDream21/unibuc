a3842dafc6a341d020b06db5600fdaae49622efc
Fabi <fabianflorete@yaho.com>
2023-11-06 19:38:11 +0200

    Tema lab PS + DAW

---
+++ AN 2/semestrul1an2/Dezvoltarea aplicatiilor Web/TemaLaborator5/TemaLaborator5/Controllers/ArticlesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TemaLaborator5.Models;
+
+namespace TemaLaborator5.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private readonly AppDbContext db;
+
+        public ArticlesController(AppDbContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            var articles = from article in db.Articles
+                           select article;
+            ViewBag.Articles = articles;
+
+            return View();
+        }
+    }
+}
---
+++ AN 2/semestrul1an2/Dezvoltarea aplicatiilor Web/TemaLaborator5/TemaLaborator5/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TemaLaborator5.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>
+        options)
+        : base(options)
+        {
+
+        }
+    public DbSet<Article> Articles { get; set; }
+    }
+}
