2a79a6bc99d9e03218b325bddfe76b5fc7ac13ab
Fabi <fabianflorete@yaho.com>
2023-11-17 14:34:29 +0200

    Lab DAW + Cablaj AF

--- AN 2/semestrul1an2/Algoritmi Fundamentali/Tema 2/cablaj.cpp
+++ AN 2/semestrul1an2/Algoritmi Fundamentali/Tema 2/cablaj.cpp
@@ -1,138 +1,96 @@
-#include <iostream>
-#include <fstream>
-#include <utility>
-#include <math.h>
-#include <vector>
-#include <algorithm>
-#include <iomanip>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 ifstream fin("cablaj.in");
 ofstream fout("cablaj.out");
 
-class DisjointSetUnion{
-private:
-    int n;
-    int* parinti;
-    int* inaltime;
-    DisjointSetUnion() = delete;
+double dist(int x1, int y1, int x2, int y2)
+{
+    return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+}
+
+class Compare
+{
 public:
-    DisjointSetUnion(int n)
-    {
-        this->n = n;
-        this->parinti = new int[n + 1];
-        this->inaltime = new int[n + 1];
-        for(int i = 0; i <= n; i++)
-        {
-            parinti[i] = i;
-            inaltime[i] = 1;
-        }
-    }
-    ~DisjointSetUnion()
-    {
-        delete[] parinti;
-        delete[] inaltime;
-    }
-
-    int find_set(int u)
-    {
-        while(parinti[u] != u)
-            u = parinti[u];
-        return u;
-    }
-
-    void unite(int u, int v)
-    {
-        int setU = find_set(u);
-        int setV = find_set(v);
-        if(inaltime[setU] > inaltime[setV])
-            parinti[setV] = setU;
-        else{
-            parinti[setU] = setV;
-            if(inaltime[setU] == inaltime[setV])
-                inaltime[setV] = inaltime[setV] + 1;
-        }
-    }
-
-    int find_distinct_sets()
-    {
-        bool cnt_multimi[n + 1];
-        int multimi_distincte = 0;
-        for(int i = 0; i <= n; i++)
-            cnt_multimi[i] = false;
-        for(int i = 0; i <= n; i++)
-            cnt_multimi[find_set(i)] = true;
-        for(int i = 0; i <= n; i++)
-            multimi_distincte += bool(cnt_multimi[i]);
-        return multimi_distincte;
+    bool operator()(pair<pair<int, int>, double> drum1, pair<pair<int, int>, double> drum2){
+        return drum1.second > drum2.second;
     }
 };
 
-double calculeaza_distanta(double x1, double y1, double x2, double y2)
-{
-    return sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
-}
-
-bool comparareDrumuri(pair<pair<int, int>, double> drum1, pair<pair<int, int>, double> drum2)
-{
-    return drum1.second < drum2.second;
-}
-
-int gasesteMultime(int* multime, int elem)
-{
-    while(multime[elem] != elem)
-        elem = multime[elem];
-    return multime[elem];
-}
 
 int main()
 {
-    vector<pair<pair<int, int>, double>> distante;
-    vector<pair<int, int>> coordonate;
+    vector<pair<int, int>> localitati;
+    priority_queue<pair<pair<int, int>, double>, vector<pair<pair<int, int>, double>>, Compare> heap;
     int n;
     fin >> n;
+    int aux = n;
+    bool cablat[n + 5];
+
     for(int i = 0; i < n; i++)
+        cablat[i] = false;
+
+    while(n)
     {
         int x, y;
         fin >> x >> y;
-        coordonate.push_back(make_pair(x, y));
+        localitati.push_back(make_pair(x, y));
+        n--;
     }
 
-    //orasele sunt in ordine incepand cu 0
-
-    for(int i = 0; i < coordonate.size() - 1; i++)
+    int next = 0, cablate = 0;
+    n = aux;
+    double total = 0;
+    while(cablate < n - 1)
     {
-        for(int j = i + 1; j < coordonate.size(); j++)
+        cablat[next] = true;
+        for(int i = 0; i < localitati.size(); i++)
         {
-            double len = calculeaza_distanta(coordonate[i].first, coordonate[i].second, coordonate[j].first, coordonate[j].second);
-            pair<int, int> drumXY(i, j);
-            pair<pair<int, int>, double> distXY(drumXY, len);
-            distante.push_back(distXY);
+            if(cablat[i])
+                continue;
+            pair<int, int> drumXY(next, i);
+            pair<pair<int, int>, double> distXY (drumXY, dist(localitati[next].first, localitati[next].second, localitati[i].first, localitati[i].second));
+            heap.push(distXY);
+        }
+
+        while(!heap.empty())
+        {
+            pair<pair<int, int>, double> elem = heap.top();
+            heap.pop();
+            int X = elem.first.first;
+            int Y = elem.first.second;
+            if(cablat[X] && cablat[Y])
+                continue;
+            if(!cablat[X])
+            {
+                cablate++;
+                total += elem.second;
+                next = X;
+                break;
+            }
+            if(!cablat[Y])
+            {
+                cablate++;
+                total += elem.second;
+                next = Y;
+                break;
+            }
         }
     }
-    sort(distante.begin(), distante.end(), comparareDrumuri);
 
-    DisjointSetUnion myset(n);
-
-    double s = 0;
-    for(int i = 0; i < distante.size(); i++)
-    {
-        //X Y => multime(X) = multime(Y)
-        int x = distante[i].first.first;
-        int y = distante[i].first.second;
-        double len = distante[i].second;
-
-        int multimeX = myset.find_set(x);
-        int multimeY = myset.find_set(y);
-        if(multimeX != multimeY)
-        {
-            s += len;
-            myset.unite(x, y);
-        }
-    }
-
-    cout << setprecision(4) << s << endl;
-
+    fout << setprecision(3) << fixed << total;
     return 0;
 }
+
+    /*pair<int, int> drumXY(1, 2);
+    pair<pair<int, int>, double> distXY(drumXY, 1.523);
+    heap.push(distXY);
+
+    pair<int, int> drumYZ(2, 10);
+    pair<pair<int, int>, double> distYZ(drumXY, 2.31);
+    heap.push(distYZ);
+
+    pair<pair<int, int>, double> val;
+    val = heap.top();
+    cout << val.second;*/
