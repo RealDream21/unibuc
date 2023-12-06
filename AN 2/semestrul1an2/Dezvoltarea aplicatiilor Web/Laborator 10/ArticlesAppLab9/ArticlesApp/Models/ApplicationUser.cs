84496a32c13c80ff7236e8c234b757570b57ebe4
Fabi <fabianflorete@yaho.com>
2023-12-06 15:49:48 +0200

    Lab 10 DAW

---
+++ AN 2/semestrul1an2/Dezvoltarea aplicatiilor Web/Laborator 10/ArticlesAppLab9/ArticlesApp/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+// PASUL 1 - useri si roluri 
+namespace ArticlesApp.Models 
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // un user poate posta mai multe comentarii
+        public virtual ICollection<Comment>? Comments { get; set; }
+
+        // un user poate posta mai multe articole
+        public virtual ICollection<Article>? Articles { get; set; }
+
+        //un user creeaza mai multe colectii
+        public virtual ICollection<Bookmark> Bookmarks { get; set; }
+
+
+        //atribute suplimentare adaugate pentru user
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set;}
+
+
+
+    }
+}
---
+++ AN 2/semestrul1an2/Dezvoltarea aplicatiilor Web/Laborator 10/ArticlesAppLab9/ArticlesApp/Models/ArticleBookmark.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ArticlesApp.Models
+{
+    //tabelul asociativ care face lagatura dintre 
+    //Article si Bookmark
+    //Un articol face parte din mai multe colectii
+    //O colectie contine mai multe articole
+    public class ArticleBookmark
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//Id-ul creste automat cand adaugam
+        public int Id { get; set; } 
+
+        public int? ArticleId {  get; set; }
+
+        public int? BookmarkId {  get; set; }
+        public DateTime BookmarkDate {  get; set; }
+
+        public virtual Article? Article {  get; set; }
+        public virtual Bookmark? Bookmark { get; set; }
+
+
+    }
+}
