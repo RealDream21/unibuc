ef543c3e1afe934fba18457b1061655412ccdee0
Fabi <fabianflorete@yahoo.com>
2023-12-22 11:10:28 +0200

    Cerinta 11 SGBD

---
+++ AN 2/semestrul1an2/SGBD/Proiect/Cerinta11(LA NIVEL DE LINIE SPER).sql
@@ -0,0 +1,21 @@
+--Fac un declansator care nu las ca valoarea unui contract sa fie marita cu mai mult de 50% din valoarea acestuia
+CREATE OR REPLACE TRIGGER trigger_cerinta_10
+    BEFORE UPDATE ON contract_cumparare
+    FOR EACH ROW
+DECLARE
+    v_exceptie EXCEPTION;
+BEGIN
+    IF(:OLD.suma + :OLD.suma*0.5) > :NEW.suma THEN
+        RAISE v_exceptie;
+    END IF;
+EXCEPTION
+    WHEN v_exceptie THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Suma noua a crescut cu mai mult de 50%');
+    DBMS_OUTPUT.PUT_LINE('sal');
+END;
+/
+
+UPDATE contract_cumparare
+SET suma = suma*0.5 + 1000
+WHERE id_primarie = 1;
+ROLLBACK;
