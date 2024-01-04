3060e3ab68ce8d5007e34160699cb8a57d8f7658
Fabi <fabianflorete@yahoo.com>
2024-01-04 11:20:47 +0200

    lab 4 DAW done

---
+++ AN 2/semestrul1an2/Dezvoltarea aplicatiilor Web/Pregatire examen/1/WebApplication2/WebApplication2/Controllers/ArticlesController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        public IActionResult Index()
+        {
+            Article[] articles = GetArticles();
+            ViewBag.Articles = articles;
+            return View();
+        }
+
+        public IActionResult Show(int? id)
+        {
+            Article[] articles = GetArticles();
+            try
+            {
+                ViewBag.Article = articles[(int)id];
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View("Error");
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult New(Article article)
+        {
+            return View("NewPostMethod");
+        }
+
+        [NonAction]
+        public Article[] GetArticles()
+        {
+            Article[] articles = new Article[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Article article = new Article();
+                article.Id = i;
+                article.Title = "Articol " + (i + 1).ToString();
+                article.Content = "Continut Articol" + (i + 1).ToString();
+                article.Date = DateTime.Now;
+                articles[i] = article;
+            }
+            return articles;
+        }
+    }
+}
