f54521b3a6913a970731dab9ea024d5a811f778a
Fabi <fabianflorete@yahoo.com>
2024-01-10 08:24:14 +0200

    daw laptop + pc

---
+++ AN 2/semestrul1an2/Dezvoltarea aplicatiilor Web/Pregatire examen/Laborator5/Laborator5/Models/AppDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Laborator5.Models
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+
+    /*
+      public AppDBContext() : base()
+      {
+      }
+
+      protected override void OnConfiguring(DbContextOptionsBuilder options)
+      {
+        options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502;Trusted_Connection=True;MultipleActiveResultSets=true");
+      }
+    */
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
