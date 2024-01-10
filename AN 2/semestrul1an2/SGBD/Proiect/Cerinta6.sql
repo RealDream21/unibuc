478b1384227d927421952684f0a4677c0ddf1b14
Fabi <fabianflorete@yahoo.com>
2024-01-10 16:09:54 +0200

    fixed cerinta 6 sgbd

--- AN 2/semestrul1an2/SGBD/Proiect/Cerinta6.sql
+++ AN 2/semestrul1an2/SGBD/Proiect/Cerinta6.sql
@@ -1,5 +1,5 @@
---Sa se mareasca contractele de cumparare cu suma >= 10.000 cu inca 10 . Afisati contractele modificate(dupa)
---din acest nou tablou sa se pastreze intr un vector doar familiile care au avut contracte cu cumparatorul cu id 1. Daca nu exista => se afiseaza un mesaj de eroare
+--Sa se mareasca contractele de cumparare cu suma >= 10.000 cu inca 1% . Afisati contractele modificate(dupa)
+--din acest nou tablou sa se pastreze intr un vector doar familiile care au avut contracte cu cumparatorul cu id 1.
 
 
 SET SERVEROUTPUT ON;
@@ -13,7 +13,8 @@ IS
     t_vector vector:=vector();
     
     TYPE tablou_imbricat IS TABLE OF contract_cumparare%rowtype;
-    t_contracte_sterse tablou_imbricat := tablou_imbricat();
+    t_contracte_totale tablou_imbricat := tablou_imbricat();
+    t_contracte_ramase tablou_imbricat := tablou_imbricat();
     
 BEGIN
     SELECT id_primarie, id_cumparator, id_familie, data_cumparare, taxa, suma BULK COLLECT INTO t_contracte
@@ -27,23 +28,29 @@ BEGIN
     END LOOP;
     
     FOR i in t_vector.first..t_vector.last LOOP
-        t_contracte_sterse.extend;
+        t_contracte_totale.extend;
         UPDATE contract_cumparare
         SET suma = suma + 0.01*suma
         WHERE id_cumparator = t_vector(i).id_cumparator
             AND id_familie = t_vector(i).id_familie
             AND data_cumparare = t_vector(i).data_cumparare
         RETURNING id_primarie, id_cumparator, id_familie, data_cumparare, taxa, suma
-        INTO t_contracte_sterse(i);
+        INTO t_contracte_totale(i);
     END LOOP;
     
-    FOR i in t_contracte_sterse.first..t_contracte_sterse.last LOOP
-        DBMS_OUTPUT.PUT_LINE(t_contracte_sterse(i).id_familie || ' ' || t_contracte_sterse(i).suma);
+    FOR i in t_contracte_totale.first..t_contracte_totale.last LOOP
+        IF(t_contracte_totale(i).id_cumparator = 1) THEN
+            t_contracte_ramase.extend;
+            t_contracte_ramase(t_contracte_ramase.count) := t_contracte_totale(i);
+        END IF;
     END LOOP;
-    
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Nu au fost gasite contracte');
+     
+     FOR i in t_contracte_ramase.first..t_contracte_ramase.last LOOP
+        DBMS_OUTPUT.PUT_LINE('Id familie: ' || t_contracte_ramase(i).id_familie || ', ' || 'suma contract: ' || t_contracte_ramase(i).suma);
+     END LOOP;
+        
+        
+  
 END p_cerinta6;
 /
 
@@ -53,3 +60,5 @@ BEGIN
     rollback;
 END;
 /
+
+SELECT * FROM contract_cumparare;
