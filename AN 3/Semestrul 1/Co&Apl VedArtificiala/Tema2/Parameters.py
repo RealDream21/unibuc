3b635217100dc9706461b23254bc0081a4ed9643
Fabi <fabianflorete@yahoo.com>
2024-12-22 14:31:46 +0200

    extragere descriptori si antrenare model

--- AN 3/Semestrul 1/Co&Apl VedArtificiala/Tema2/Parameters.py
+++ AN 3/Semestrul 1/Co&Apl VedArtificiala/Tema2/Parameters.py
@@ -13,13 +13,13 @@ def show_image(title, image):
     cv.destroyAllWindows()
 
 def intersectie_dreptunghi(d1, d2):
-    d1_x_top_left, d1_y_top_left, d1_x_bottom_right, d1_x_bottom_right = d1
+    d1_x_top_left, d1_y_top_left, d1_x_bottom_right, d1_y_bottom_right = d1
     d2_x_top_left, d2_y_top_left, d2_x_bottom_right, d2_y_bottom_right = d2
 
     return (d1_x_top_left <= d2_x_bottom_right 
             and d1_x_bottom_right >= d2_x_top_left
             and d1_y_top_left <= d2_y_bottom_right
-            and d1_x_bottom_right >= d2_y_top_left)
+            and d1_y_bottom_right >= d2_y_top_left)
 
 class Annotation():
     def __init__(self, image_name, x_top_left, y_top_left, x_bottom_right, y_bottom_right, person):
@@ -34,15 +34,46 @@ class Annotation():
         return f'Person {self.person} is in image {self.image_name}, starting at ({self.x_top_left}, {self.y_top_left}) and ending at ({self.x_bottom_right}, {self.y_bottom_right})'
 
 class Parameters:
-    def __init__(self, OX_dim_window: int, OY_dim_window: int, extract_examples: bool, verbose:bool = False): # L = lungime, W = inaltime
+    def __init__(self, OX_dim_window: int, OY_dim_window: int, extract_examples: bool, dim_hog_cell: int, dim_descriptor_cell: int, overlap, threshold, use_flip_images: bool, n_negative_examples, verbose:bool = False): # L = lungime, W = inaltime
+        self.OX_dim_window = OX_dim_window
+        self.OY_dim_window = OY_dim_window
+        
+        self.dim_hog_cell = dim_hog_cell
+        self.dim_descriptor_cell = dim_descriptor_cell
+        self.overlap = overlap
+        self.threshold = threshold
+        self.use_flip_images = use_flip_images
+        self.n_negative_examples = n_negative_examples # so that we don't use all 500.000 examples. Some are similar
+
         self.base_dir = os.getcwd()
         self.my_folder = 'proprii'
         self.positive_train_folder = f'Positive{OX_dim_window}x{OY_dim_window}' #folder cu exemple positive de dimensiune L_window x W_window
         self.negative_train_folder = f'Negative{OX_dim_window}x{OY_dim_window}' #folder cu exemple negative de dimensiune L_window x W_window
+        self.positive_train_folder_path = os.path.join(os.path.join(self.base_dir, self.my_folder), self.positive_train_folder)
+        self.negative_train_folder_path = os.path.join(os.path.join(self.base_dir, self.my_folder), self.negative_train_folder)
+       
+        self.models_folder = 'model_saves'
+        self.models_folder_path = os.path.join(self.my_folder, self.models_folder)
+        self.this_model_save_files = f"model_{OX_dim_window}x{OY_dim_window}"
+        self.this_model_save_files_path = os.path.join(self.models_folder_path, self.this_model_save_files)
+        
+        self.test_folder = 'antrenare'
+        self.test_folder_path = os.path.join(self.base_dir, self.test_folder)
+
+        self.descriptors_folder = 'descriptors'
+        self.descriptors_folder_path = os.path.join(self.my_folder, self.descriptors_folder)
+        self.positive_descriptors_path = os.path.join(self.descriptors_folder_path, f'descriptoriPozitivi_{self.dim_hog_cell}_{self.dim_descriptor_cell}_{self.OX_dim_window}x{self.OY_dim_window}.npy')
+        self.negative_descriptors_path = os.path.join(self.descriptors_folder_path, f'descriptoriNegativi_{self.dim_hog_cell}_{self.dim_descriptor_cell}_{self.OX_dim_window}x{self.OY_dim_window}.npy')
         
         os.makedirs(self.my_folder, exist_ok=True)
         os.makedirs(os.path.join(self.my_folder, self.positive_train_folder), exist_ok=True)
         os.makedirs(os.path.join(self.my_folder, self.negative_train_folder), exist_ok=True)
+        os.makedirs(self.models_folder_path, exist_ok=True)
+        os.makedirs(self.this_model_save_files_path, exist_ok=True)
+        os.makedirs(self.descriptors_folder_path, exist_ok=True)
+
+        if self.n_negative_examples == -1:
+            self.n_negative_examples = os.listdir(self.negative_train_folder_path)
 
         if extract_examples:
             train_folder = os.path.join(self.base_dir, 'antrenare')
@@ -52,53 +83,51 @@ class Parameters:
                 annotations = [x.strip().split() for x in g.readlines()]
                 annotations = [Annotation(x[0], x[1], x[2], x[3], x[4], x[5]) for x in annotations]
 
-                # for annotation in annotations: #redraw the square so that it fits into L_window, W_window. Extract positive examples
-                #     image = cv.imread(os.path.join(os.path.join(train_folder, folder), annotation.image_name), cv.IMREAD_GRAYSCALE)
-                #     patch = image[annotation.y_top_left: annotation.y_bottom_right, annotation.x_top_left: annotation.x_bottom_right]
-                #     patch = cv.resize(patch, (OX_dim_window, OY_dim_window))
-                #     cv.imwrite(os.path.join(os.path.join(os.path.join(self.base_dir, 'proprii'), self.positive_train_folder), f'{annotation.person}_{folder}Dataset_{annotation.image_name}'), patch)
-                #     if verbose:
-                #         show_image('img', patch.copy())
+                for annotation in annotations: #redraw the square so that it fits into L_window, W_window. Extract positive examples
+                    image = cv.imread(os.path.join(os.path.join(train_folder, folder), annotation.image_name), cv.IMREAD_GRAYSCALE)
+                    patch = image[annotation.y_top_left: annotation.y_bottom_right, annotation.x_top_left: annotation.x_bottom_right]
+                    patch = cv.resize(patch, (OY_dim_window, OX_dim_window))
+                    cv.imwrite(os.path.join(os.path.join(os.path.join(self.base_dir, 'proprii'), self.positive_train_folder), f'{annotation.person}_{folder}Dataset_{annotation.image_name}'), patch)
+                    if verbose:
+                        show_image('img', patch.copy())
                 
-
                 annotation_dict = dict([(x.image_name, []) for x in annotations])
                 for x in annotations:
                     annotation_dict[x.image_name].append(x)
  
-                step = 5
+                step = 10
                 cnt = 0
                 for image_name in os.listdir(os.path.join(train_folder, folder)):
                     image = cv.imread(os.path.join(os.path.join(train_folder, folder), image_name), cv.IMREAD_GRAYSCALE)
                     annotations = annotation_dict[image_name]
                     if verbose:
                         for an in annotations:
-                            cv.rectangle(image, (an.x_top_left, an.y_top_left), (an.x_bottom_right, an.y_bottom_right), (0, 255, 255), 3)
-                    for y in range(0, image.shape[1] - OY_dim_window, step):
-                        for x in range(0, image.shape[0] - OX_dim_window, step):
+                            cv.rectangle(image.copy(), (an.x_top_left, an.y_top_left), (an.x_bottom_right, an.y_bottom_right), (0, 255, 255), 3)
+                    for x in range(0, image.shape[0] - OX_dim_window, step):
+                        for y in range(0, image.shape[1] - OY_dim_window, step):
+
+                            if verbose:
+                                patch1 = image[x: x + OX_dim_window, y: y + OY_dim_window].copy()
+                                show_image('patch extras', patch1)
                             ok = True
                             for annotation in annotations:
-                                if intersectie_dreptunghi((x, y, x + OX_dim_window, y + OY_dim_window), 
+                                if intersectie_dreptunghi((y, x, y + OY_dim_window, x + OX_dim_window), 
                                                           (annotation.x_top_left, annotation.y_top_left, annotation.x_bottom_right, annotation.y_bottom_right)):
                                     ok = False
-                                    patch1 = image[x: x + OX_dim_window, x: x + OX_dim_window]
-                                    patch2 = image[annotation.x_top_left:annotation.x_bottom_right, annotation.y_top_left: annotation.y_bottom_right]
-                                    if y % 2 == 0 and verbose:
-                                        show_image('img', patch1)
-                                        show_image('img', patch2)
                                     break
                             if ok == False:
                                 continue
 
-                            patch = image[x: x + OX_dim_window, x: x + OX_dim_window]
-                            cv.rectangle(image,(x, y), (x + OX_dim_window, y + OY_dim_window), (0, 255, 0), 3)
-
+                            try:
+                                patch = image[x: x + OX_dim_window, y: y + OY_dim_window].copy()
+                            except:
+                                print(x, x + OX_dim_window, image.shape)
+                            
                             if verbose:
+                                cv.rectangle(image, (y, x), (y + OY_dim_window, x + OX_dim_window), (0, 255, 0), 3)
                                 show_image('img', image)
-
-                            if verbose:
                                 show_image('img', patch)
-                            #cv.imwrite(os.path.join(os.path.join(os.path.join(self.base_dir, 'proprii'), self.negative_train_folder), f'{cnt}.jpg'), patch)
+
+                            cv.imwrite(os.path.join(os.path.join(os.path.join(self.base_dir, 'proprii'), self.negative_train_folder), f'{cnt}.jpg'), patch)
                             cnt += 1
-
-
 
--- AN 3/Semestrul 1/Co&Apl VedArtificiala/Tema2/main.py
+++ AN 3/Semestrul 1/Co&Apl VedArtificiala/Tema2/main.py
@@ -1,3 +1,33 @@
 from Parameters import *
+from FacialDetector import *
 
-a = Parameters(64, 128, True, True)
+#a = Parameters(64, 128, True, False) #64 coloane, 128 linii
+#a = Parameters(128, 128, True, False)
+#b = Parameters(128, 64, True, False)
+
+params1 = Parameters(OX_dim_window=64, OY_dim_window=128, extract_examples=False, dim_hog_cell=12, dim_descriptor_cell=36, overlap=0.3, threshold=0, use_flip_images=True, n_negative_examples=100000)
+
+facialDetector1 = FacialDetector(params1)
+
+positive_features_path1 = params1.positive_descriptors_path
+if os.path.exists(positive_features_path1):
+    positive_features1 = np.load(positive_features_path1)
+else:
+    positive_features1 = facialDetector1.get_positive_descriptors()
+    np.save(positive_features_path1, positive_features1)
+
+negative_features_path1 = params1.negative_descriptors_path
+if os.path.exists(negative_features_path1):
+    negative_features1 = np.load(negative_features_path1)
+else:
+    negative_features1 = facialDetector1.get_negative_descriptors()
+    np.save(negative_features_path1, negative_features1)
+
+
+X = np.concatenate((np.squeeze(positive_features1), np.squeeze(negative_features1)), axis=0)
+y = np.concatenate((np.ones(positive_features1.shape[0]), np.zeros(negative_features1.shape[0])))
+facialDetector1.train_classifier(X, y)
+
+detections, scores, file_names = facialDetector1.run()
+
+print(detections, scores, file_names)
